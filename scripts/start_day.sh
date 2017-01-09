66f46e1e63ada603a43b9a4a8c1c431d01175f06
bbjunaid <bilal.junaid@prezi.com>
2017-01-08 23:12:32 -0800

    [start day] Flush redis first

--- scripts/start_day.sh
+++ scripts/start_day.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Flushing redis"
+/usr/local/bin/redis-cli flushall
+
 echo "Downloading engagement file if exists"
 source /Users/bilaljunaid/Dropbox/Full_Time_Jobs/code_practice/stocks/virtualenv/bin/activate
 python /Users/bilaljunaid/Dropbox/Full_Time_Jobs/code_practice/stocks/download_engagement.py
-
-echo "Flushing redis"
-/usr/local/bin/redis-cli flushall
