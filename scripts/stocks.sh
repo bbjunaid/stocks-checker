9246ca1019e776a5d071ff6b347bbe5b936095f0
bbjunaid <bilal.junaid@prezi.com>
2017-01-08 17:44:14 -0800

    [redis] Different way to start on linux

--- scripts/stocks.sh
+++ scripts/stocks.sh
@@ -6,7 +6,7 @@ then
     echo "Redis is running"
 else
     echo "Starting Redis"
-    /usr/local/bin/redis-server --daemonize yes
+    /usr/local/bin/redis-server --daemonize yes || sudo redis-server /etc/redis.conf
 fi
 
 source /Users/bilaljunaid/Dropbox/Full_Time_Jobs/code_practice/stocks/virtualenv/bin/activate
