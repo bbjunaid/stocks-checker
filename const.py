0cb4fca635bd9538c9c4acd89b53c2fe9bb80849
bbjunaid <bilal.junaid@prezi.com>
2017-01-18 02:25:47 -0800

    [const] Update

--- const.py
+++ const.py
@@ -1,16 +1,6 @@
-GMAIL_USER = 'bbjunaid@gmail.com'
-GMAIL_PASS = ''
-GMAIL_SEND_LIST = ["bbjunaid@gmail.com", "obstruktion@gmail.com"]
-# GMAIL_SEND_LIST = ["bbjunaid@gmail.com"]
-
-TWITTER_STOCK_USERNAME = 'fitzstockcharts'
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-TWITTER_ACCESS_TOKEN_KEY = ''
-TWITTER_ACCESS_TOKEN_SECRET = ''
-
-CACHE_EXPIRY_TIME = 8*60*60
+CACHE_EXPIRY_TIME = 10*60*60
 CACHE_KEY_TWITTER_STATUS = 'twitter-reference-status-id'
 CACHE_KEY_TREND = 'trend'
 
-ENGAGEMENT_FILE_PATH = "/Users/bilaljunaid/Google Drive/Stocks/engagement.xls"
+ENGAGEMENT_FILE_PATH = "./engagement.xls"
+FOLLOWERS_FILE_PATH = "./followers.txt"
