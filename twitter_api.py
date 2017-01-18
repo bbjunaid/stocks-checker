13ba13e0421a7e7a6c8ba1fa3148462a5a5efefb
bbjunaid <bilal.junaid@prezi.com>
2017-01-18 02:09:50 -0800

    [twitter] Dump followers of fitz

--- twitter_api.py
+++ twitter_api.py
@@ -2,10 +2,11 @@
 
 import calendar
 import time
+import pickle
 
 import twitter
 
-from const import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN_KEY, TWITTER_ACCESS_TOKEN_SECRET, TWITTER_STOCK_USERNAME
+from const import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN_KEY, TWITTER_ACCESS_TOKEN_SECRET, TWITTER_STOCK_USERNAME, FOLLOWERS_FILE_PATH
 
 api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
                   consumer_secret=TWITTER_CONSUMER_SECRET,
@@ -29,6 +30,30 @@ def get_status_for_first_tweet_of_day():
             break
 
     return reference_status
+
+
+def get_followers():
+    followers_list = []
+    follower_ids = api.GetFollowerIDs(screen_name=TWITTER_STOCK_USERNAME)
+    follower_ids_chunks = chunks(follower_ids, 100)
+
+    for item in follower_ids_chunks:
+        followers = api.UsersLookup(user_id=item)
+        for follower in followers:
+            followers_list.append(follower.name)
+
+    return followers_list
+
+
+def dump_followers_to_file():
+    with open(FOLLOWERS_FILE_PATH, 'wb') as fp:
+        pickle.dump(get_followers(), fp)
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 
 if __name__ == "__main__":
