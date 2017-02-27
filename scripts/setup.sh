71727bcfbc901e4cc5557e19953d2ec5c035cc40
bbjunaid <bilal.junaid@prezi.com>
2017-02-26 23:50:20 -0800

    [scripts] Change scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,4 +8,5 @@ virtualenv virtualenv
 
 source virtualenv/bin/activate
 
+env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include"
 pip install -r requirements.txt
--- scripts/start_day.sh
+++ scripts/start_day.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Check if gedit is running
+if pgrep "redis-server" > /dev/null
+then
+    echo "Redis is running"
+else
+    echo "Starting Redis"
+    /usr/local/bin/redis-server --daemonize yes || sudo redis-server /etc/redis.conf
+fi
+
 echo "Flushing redis"
 /usr/local/bin/redis-cli flushall
 
